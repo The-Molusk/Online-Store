b60ead7908d647cb5a9682b2fa89eab3f68600ed
Milosz Wedrowski <93678410+Tidod@users.noreply.github.com>
2024-10-18 16:33:40 +0100

    Showcase + Resource files
    Added showcase/testing videos along with extra files required for the program to function.

--- WinForms Prot1/obj/Debug/netcoreapp3.1/WinForms Prot1.AssemblyInfo.cs
+++ WinForms Prot1/obj/Debug/netcoreapp3.1/WinForms Prot1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WinForms Prot1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d7c9fe19759a6902107aadbd9926908b1c8c4ef")]
 [assembly: System.Reflection.AssemblyProductAttribute("WinForms Prot1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WinForms Prot1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
